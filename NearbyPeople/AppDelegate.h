159b645dae37e10e54d743b55145cff3d0206a60
Andrewliu <andrewliu20@163.com>
2016-07-13 13:56:00 +0800

    Initial Commit

---
+++ NearbyPeople/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NearbyPeople
+//
+//  Created by andrewliu on 16/7/13.
+//  Copyright © 2016年 andrewliu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NearbyPeople/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NearbyPeople
+//
+//  Created by andrewliu on 16/7/13.
+//  Copyright © 2016年 andrewliu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
